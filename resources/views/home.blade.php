721e1eedea9f7f6d48670f7b7e544f7512558ee8
sokcellerie <vmangwandjo@gmail.com>
2024-03-29 00:22:15 +0100

    add authentification and create todos table

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="container text-center">
+            <div class="row justify-content-center">
+              <div class="col-auto">
+                <a href="" class="btn btn-primary">+Creer une tache</a>
+              </div>
+              <div class="col-auto">
+                <a href=""  class="btn btn-secondary">Toutes les Taches</a>
+              </div>
+              <div class="col-auto">
+                <a href=""  class="btn btn-warning">En cours</a>
+              </div>
+              <div class="col-auto">
+                <a href="" class="btn btn-dark">Terminées</a>
+              </div>
+            </div>
+          </div>
+    </div>
+</div>
+@endsection
